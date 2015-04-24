85a13107c0a622e1c3cbf1323c9b441942d50565
jason137 <jason.dolatshahi@gmail.com>
2015-04-24 00:48:36 -0400

    fixing linkedin link

--- haskell/test.hs
+++ haskell/test.hs
@@ -23,3 +23,15 @@ qsort (x: xs) = qsort smaller ++ [x] ++ qsort larger
     where
         smaller = [a | a <- xs, a <= x]
         larger = [b | b <- xs, b > x]
+
+factors :: Int -> [Int]
+factors n = [k | k <- [1..n], n `mod` k == 0]
+
+prime :: Int -> Bool
+prime n = factors n == [1, n]
+
+primes_below :: Int -> [Int]
+primes_below n = [k | k <- [2..n], prime k]
+
+prime_facs :: Int -> [Int]
+prime_facs n = [k | k <- factors n, k <= sqrt fromIntegral n, prime k]
